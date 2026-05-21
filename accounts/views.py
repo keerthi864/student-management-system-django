631c1ec3f0c6009ada827a6846a13a5abe54af4d
Keerthana <vkeerthana180604@gmail.com>
2026-05-21 23:13:22 +0530

    fixed register username validation

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 def login_page(request):
 
@@ -35,8 +36,11 @@ def register_page(request):
     if request.method == "POST":
 
         username = request.POST.get('username')
-
         password = request.POST.get('password')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already exists')
+            return redirect('/register/')
 
         User.objects.create_user(
             username=username,
